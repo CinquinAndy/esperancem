1ba7df432417ad3e0d080f7ced39a996001ed426
CinquinAndy <cinquin.andy@gmail.com>
2025-06-15 22:38:47 +0200

    fix: remove unused MailIcon component from social links scope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,6 @@ import {
 } from '@/components/SocialIcons'
 import bookCover from '@/images/photos/cover_on_book.jpg'
 
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-	return (
-		<svg
-			viewBox='0 0 24 24'
-			fill='none'
-			strokeWidth='1.5'
-			strokeLinecap='round'
-			strokeLinejoin='round'
-			aria-hidden='true'
-			{...props}
-		>
-			<path
-				d='M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z'
-				className='fill-zinc-100/10 stroke-zinc-500'
-			/>
-			<path
-				d='m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6'
-				className='stroke-zinc-500'
-			/>
-		</svg>
-	)
-}
-
 function SocialLink({
 	icon: Icon,
 	...props
