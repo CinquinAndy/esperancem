7217ab091a7b05c6d8873818d02a3722db6b613e
CinquinAndy <cinquin.andy@gmail.com>
2025-06-15 19:52:44 +0200

    feat: add configuration and documentation files for tooling and development

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import perfectionist from 'eslint-plugin-perfectionist'
+import { defineConfig } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	allConfig: js.configs.all,
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default defineConfig([
+	{
+		extends: compat.extends('next/core-web-vitals', 'next/typescript'),
 
-export default eslintConfig;
+		plugins: {
+			perfectionist,
+		},
+
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'no-console': [
+				'error',
+				{
+					allow: ['warn', 'error', 'info'],
+				},
+			],
+
+			'perfectionist/sort-enums': 'error',
+			'perfectionist/sort-imports': ['error'],
+			'perfectionist/sort-objects': 'error',
+			'perfectionist/sort-variable-declarations': 'error',
+		},
+
+		settings: {
+			perfectionist: {
+				partitionByComment: false,
+				partitionByNewLine: false,
+				type: 'alphabetical',
+			},
+		},
+	},
+])
